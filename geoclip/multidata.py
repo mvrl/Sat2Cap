d987c28ae4e0657161c0fa64f52c7d19124f46a7
Dhakal <a.dhakal@compute1-client-1.ris.wustl.edu>
2023-02-23 19:27:13 -0600

    changed webdataset to return pillow images

--- geoclip/multidata.py
+++ geoclip/multidata.py
@@ -5,50 +5,61 @@ from torchvision import transforms
 import scipy
 from PIL import Image
 import code
+from argparse import Namespace
+from .clip import Clip
+
 
 class MultiData(object):
 
-    def __init__(self, wds_path):
+    def __init__(self,args):
         self.img_size = 224
-        self.wds_path = wds_path
+        self.args = args
 
     def get_ds(self,mode):
         print(f'Initializing {mode} dataset')
         if mode=='train':
-            self.dataset = wds.WebDataset(self.wds_path, resampled=True)
-            self.dataset = self.dataset.shuffle(1000).decode('rgb').to_tuple("groundlevel.jpg", "overhead.jpg", "metadata.json","__key__")
+            self.dataset = wds.WebDataset(self.args.train_path, resampled=True)
+            self.dataset = self.dataset.shuffle(1000).decode('pil').to_tuple("groundlevel.jpg", "overhead.jpg", "metadata.json","__key__").map(self.do_transforms).batched(self.args.train_batch_size).with_epoch(self.args.train_epoch_length)
         elif mode=='test':
-            self.dataset = wds.WebDataset(self.wds_path)
-            self.dataset = self.dataset.decode('rgb').to_tuple("groundlevel.jpg", "overhead.jpg", "metadata.json","__key__")
-        self.dataset = self.dataset.map(self.do_transforms)
+            self.dataset = wds.WebDataset(self.args.vali_path, resampled=True)
+            self.dataset = self.dataset.decode('pil').to_tuple("groundlevel.jpg", "overhead.jpg", "metadata.json","__key__").map(self.do_transforms).batched(self.args.val_batch_size).with_epoch(self.args.val_epoch_length)
         return self.dataset
 
 
-    def to_pil(self, batched_img):
-        pils = [Image.from_array(img) for img in batched_img]
-        return pils
-
     def do_transforms(self, sample):
         img, imo, json, key = sample
-        self.transforms_img = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize(size=(224,224), interpolation=transforms.InterpolationMode.BILINEAR)
-        ])
-        
-        self.transforms_imo = transforms.Compose([
-            transforms.ToTensor()
-        ])
 
-        img = self.transforms_img(img)
-        imo = self.transforms_imo(imo)
+        img = img.resize((224,224), resample=Image.BICUBIC)
+        # self.transforms_img = transforms.Compose([
+        #     transforms.Resize(size=(224,224), interpolation=transforms.InterpolationMode.BICUBIC)
+        #     transforsm.CenterCrop(size=(224,224)),
+        #     transforms.ToTensor()
+        # ])
+        
+        # self.transforms_imo = transforms.Compose([
+        #     transforms.Resize(size=(800,800), interpolation=transforms.InterpolationMode.BICUBIC)
+        #     transforms.CenterCrop(size=(800,800)),
+        #     transforms.ToTensor()
+        # ])
+
+        # img = self.transforms_img(img)
+        # imo = self.transforms_imo(imo)
         return img, imo, json, key
+
 
 if __name__ == '__main__':
     wds_path = '/home/a.dhakal/active/datasets/YFCC100m/webdataset/0a912f85-6367-4df4-aafe-b48e6e1d2be4.tar'
-    dataset = MultiData(wds_path).get_ds()
-    #code.interact(local=dict(globals(), **locals()))
+    args = {'train_path':wds_path, 'train_batch_size':16, 'train_epoch_length':10, 'normalize_embeddings':True}
+
+    args = Namespace(**args)
+    dataset = MultiData(args).get_ds('train')
+    
     sample = next(iter(dataset))
     img, imo, _, _ = sample
-    print(img.shape, imo.shape)
+    print(len(img), len(imo))
 
+    clip_overhead = Clip(args,'overhead')
+    clip_ground = Clip(args,'ground_level')
+
+    code.interact(local=dict(globals(), **locals()))
 #   
