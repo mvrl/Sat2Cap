c46dfbc6ba1b56ebc7d3e2dcd424a6e8871a34e0
Dhakal <a.dhakal@compute1-client-1.ris.wustl.edu>
2023-03-10 00:41:41 -0600

    Added code for interpolated models

--- geoclip/evaluations/test_retrieval.py
+++ geoclip/evaluations/test_retrieval.py
@@ -13,16 +13,17 @@ from .metrics import Retrieval
 def get_args():
     parser = ArgumentParser(description='arguments for runnning retrieval metrics', formatter_class=RawTextHelpFormatter)
 
-    parser.add_argument('--ckpt_path', type=str, default='/home/a.dhakal/active/user_a.dhakal/geoclip/logs/GeoClip/4sbnqb88/checkpoints/epoch=0-step=3600-top_k_score=0.838.ckpt')
+    parser.add_argument('--ckpt_path', type=str, default='/home/a.dhakal/active/user_a.dhakal/geoclip/logs/GeoClip/st07vzqb/checkpoints/epoch=0-step=2500-top_k_score=0.820.ckpt')
     parser.add_argument('--test_path', type=str, default='/home/a.dhakal/active/datasets/YFCC100m/webdataset/9f248448-1d13-43cb-a336-a7d92bc5359e.tar')
     parser.add_argument('--batch_size', type=int, default=1000)
-        
+    parser.add_argument('--device', type=str, default='cpu')
+    parser.add_argument('--geoclip_wt', type=float, default=0.5)
     args = parser.parse_args()
     return args
 
+
 def get_retrieval_metric(model, sample, k=1):
     data_size = len(sample)
-    
     embeddings = model(sample)
     print('Output keys:',embeddings.keys())
     ground_img_embeddings = embeddings['ground_img_embeddings']
@@ -33,10 +34,9 @@ def get_retrieval_metric(model, sample, k=1):
     metric = retrieval.fit_k_similar(overhead_img_embeddings, ground_img_embeddings)
     return metric
 
-
 if __name__ == '__main__':
     args = get_args() 
-    device = torch.device('cpu')
+    device = torch.device(args.device)
     #no gradient context manager for evaluation
     with torch.set_grad_enabled(False):
         #load pretrained weights
@@ -45,46 +45,41 @@ if __name__ == '__main__':
     
             #set new hyper parameters
             hparams['val_batch_size'] = args.batch_size
-            hparams['test_path'] = args.test_path   
-            
+            hparams['test_path'] = args.test_path               
             geoclip = GeoClip(hparams=hparams).eval().to(device)
 
             #set requires grad to false
             for param in geoclip.parameters():
                 param.requires_grad=False
             geoclip.load_state_dict(checkpoint['state_dict'])
+
             #fetch the test dataloader
             val_dataloader = geoclip.val_dataloader()
             sample = next(iter(val_dataloader))
-            # embeddings = geoclip(sample)
-            # print('Output keys:',embeddings.keys())
-            # ground_img_embeddings = embeddings['ground_img_embeddings']
-            # overhead_img_embeddings = embeddings['overhead_img_embeddings']
-            # keys = embeddings['keys']
-            # retrieval = Retrieval(k=1)
-            # metric = retrieval.fit_k_similar(overhead_img_embeddings, ground_img_embeddings)
+
             #compute the metric for baseline CLIP
             baseline_clip = GeoClip(hparams=hparams).eval().to(device)
             for param in baseline_clip.parameters():
                 param.requires_grad=False
 
+            
+            #get the interpolated model
+            baseline_dict = baseline_clip.state_dict()
+            trained_dict = geoclip.state_dict()
+
+            for key in baseline_dict:
+                trained_dict[key] = (1-args.geoclip_wt)*baseline_dict[key]+args.geoclip_wt*trained_dict[key]
+
+            interpolated_model = GeoClip(hparams=hparams).eval().to(device)
+            interpolated_model.load_state_dict(trained_dict)
+            for param in interpolated_model.parameters():
+                param.requires_grad=False
+        
             geoclip_metric = get_retrieval_metric(geoclip, sample, 5)
             print(f'The retrieval metric for geoclip is {geoclip_metric}')
-            baseline_metric = get_retrieval_metric(baseline_clip, sample, 1)
+            baseline_metric = get_retrieval_metric(baseline_clip, sample, 5)
             print(f'The retrieval metric for baseline is {baseline_metric}')
-
+            interpolated_metric = get_retrieval_metric(interpolated_model, sample, 5)
+            print(f'The retrieval metric for interpolated model is {interpolated_metric}')
             
-    code.interact(local=dict(globals(), **locals()))
-
-# with torch.set_grad_enabled(False):
-#     sample = next(iter(test_dataloader))
-#     embeddings = geoclip(sample)
-#     print('Output keys:',embeddings.keys())
-#     ground_img_embeddings = embeddings['ground_img_embeddings']
-#     overhead_img_embeddings = embeddings['overhead_img_embeddings']
-#     keys = embeddings['keys']
-#     retrieval = Retrieval(k=10)
-#     metric = retrieval.fit_k_similar(overhead_img_embeddings, ground_img_embeddings)
-#     print(metric)
-
-    
+# code.interact(local=dict(globals(), **locals()))
--- geoclip/evaluations/zero_shot.py
+++ geoclip/evaluations/zero_shot.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from tqdm import tqdm
 import random
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawTextHelpFormatter
 #pytorch imports
 import torch
 import torch.nn as nn
@@ -22,29 +22,22 @@ from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
 from ..geoclip import GeoClip
 
 class ZeroShot(nn.Module):
-    def __init__(self, queries, ckpt_path):
+    def __init__(self,hparams):
         super().__init__()
-        self.queries = queries
-        self.ckpt_path = ckpt_path
-        if ckpt_path=='baseline':
-            self.baseline_vit = 'openai/clip-vit-base-patch32'
-            print('Running off the shelf CLIP model')
-            self.base_model, self.base_processor = self.get_baseline_clip()
+        self.hparams = hparams
+
+        self.baseline_vit = 'openai/clip-vit-base-patch32'
+        
+        if self.hparams.do_interpolate:
+            self.geoclip = self.get_interpolate()
         else:
-            self.baseline_vit = 'openai/clip-vit-base-patch32'
-            print('Running pretrained GeoClip model')
             self.geoclip = self.get_geoclip()
         self.query_embeddings = self.get_query_embeddings()
     
     def forward(self, batch):
         x,y = batch
-        if self.ckpt_path == 'baseline':
-            processed_image = self.base_processor(list(x), return_tensors='pt', padding=True)
-            batch_tensors = self.base_model(**processed_image)
-            batch_embeddings = batch_tensors.image_embeds
-            normalized_imo_embeddings = batch_embeddings/batch_embeddings.norm(p=2,dim=-1, keepdim=True)
-        else:
-            normalized_imo_embeddings = self.geoclip.forward(list(x)).detach()
+
+        normalized_imo_embeddings = self.geoclip.forward(list(x)).detach()
         similarity_matrix = torch.matmul(normalized_imo_embeddings, self.query_embeddings.t())
        # print(f'Similarity Matrix Shape is {similarity_matrix.shape}')
         _, max_indices = torch.max(similarity_matrix, axis=-1)
@@ -53,7 +46,7 @@ class ZeroShot(nn.Module):
     #return the CLIP embeddinds for the given queries using pretrained CLIP Text Model
     def get_query_embeddings(self):
         text_tokenizer = AutoTokenizer.from_pretrained(self.baseline_vit)
-        processed_queries = text_tokenizer(self.queries, padding=True, return_tensors='pt')
+        processed_queries = text_tokenizer(self.hparams.queries, padding=True, return_tensors='pt')
         text_model = CLIPTextModelWithProjection.from_pretrained(self.baseline_vit).eval()
         for params in text_model.parameters():
             params.requires_grad = False
@@ -65,24 +58,57 @@ class ZeroShot(nn.Module):
     #load the overhead img encoder from a checkpoint path
     def get_geoclip(self):
         #load geoclip model from checkpoint
-        pretrained_ckpt = torch.load(self.ckpt_path)
-        hparams = pretrained_ckpt['hyper_parameters']
-        hparams['vit'] = '32' ##need to use this as this attribute was not set when training the current checkpoint
-        pretrained_weights = pretrained_ckpt['state_dict']
-        model = GeoClip(hparams)
-        model.load_state_dict(pretrained_weights)
-        geoclip = model.imo_encoder.eval()
+        pretrained_ckpt = torch.load(self.hparams.ckpt_path)
+        pretrained_hparams = pretrained_ckpt['hyper_parameters']
+        model = GeoClip(pretrained_hparams)
+        if self.hparams.do_baseline:
+            geoclip = model.imo_encoder.eval()
+        
+        else:
+            pretrained_weights = pretrained_ckpt['state_dict']
+            model.load_state_dict(pretrained_weights)
+            geoclip = model.imo_encoder.eval()
+        
         #set all requires grad to false
         for params in geoclip.parameters():
             params.requires_grad=False
         return geoclip 
 
-    def get_baseline_clip(self):
-        image_processor = CLIPImageProcessor.from_pretrained(self.baseline_vit)
-        vision_model = CLIPVisionModelWithProjection.from_pretrained(self.baseline_vit).eval()
-        for params in vision_model.parameters():
+    def get_interpolate(self):
+        print('Running Interpolated Model')
+        pretrained_ckpt = torch.load(self.hparams.ckpt_path)
+        pretrained_hparams = pretrained_ckpt['hyper_parameters']
+        pretrained_weights = pretrained_ckpt['state_dict']
+        
+        model_baseline = GeoClip(pretrained_hparams)
+        model_trained = GeoClip(pretrained_hparams)
+        model_trained.load_state_dict(pretrained_weights)
+        
+        baseline_geoclip = model_baseline.imo_encoder.eval()
+        trained_geoclip = model_trained.imo_encoder.eval()
+
+        for params in baseline_geoclip.parameters():
             params.requires_grad=False
-        return vision_model, image_processor
+
+        for params in trained_geoclip.parameters():
+            params.requires_grad=False
+
+        baseline_dict = baseline_geoclip.state_dict()
+        trained_dict = trained_geoclip.state_dict()
+
+        for key in baseline_dict:
+            trained_dict[key] = (1-args.geoclip_wt)*baseline_dict[key]+args.geoclip_wt*trained_dict[key]
+
+        interpolated_model = GeoClip(pretrained_hparams)
+        interpolated_geoclip = interpolated_model.imo_encoder.eval()
+        
+        for params in interpolated_geoclip.parameters():
+            params.requires_grad=False
+        
+        interpolated_geoclip.load_state_dict(trained_dict)
+        
+        return interpolated_geoclip
+
 
 def set_seed(seed: int = 56) -> None:
     np.random.seed(seed)
@@ -96,25 +122,38 @@ def set_seed(seed: int = 56) -> None:
     os.environ["PYTHONHASHSEED"] = str(seed)
     print(f"Random seed set as {seed}")
 
+def get_args():
+    parser = ArgumentParser(description='', formatter_class=RawTextHelpFormatter)
+    
+    #hparams
+    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--dataset_path', type=str, default='/home/a.dhakal/active/user_a.dhakal/datasets/RESISC45/NWPU-RESISC45')
+    parser.add_argument('--batch_size', type=str, default=128)
+    parser.add_argument('--train_size', type=float, default=0.9)
+    parser.add_argument('--ckpt_path', type=str, default='/home/a.dhakal/active/user_a.dhakal/geoclip/logs/GeoClip/st07vzqb/checkpoints/epoch=0-step=2500-top_k_score=0.820.ckpt')
+    parser.add_argument('--do_baseline', action='store_true', default=False)
+    parser.add_argument('--do_interpolate', action='store_true', default=False)
+    parser.add_argument('--geoclip_wt', type=float, default=0.5)
+
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
     #set tokenizer parallelism to true
     os.environ["TOKENIZERS_PARALLELISM"] = "true"
     #params
     set_seed(24)
-    dataset_path = '/home/a.dhakal/active/user_a.dhakal/datasets/RESISC45/NWPU-RESISC45'
-    ckpt_path = '/home/a.dhakal/active/user_a.dhakal/geoclip/logs/GeoClip/4sbnqb88/checkpoints/epoch=0-step=3600-top_k_score=0.838.ckpt' ## vit-32 B
-    #ckpt_path = 'baseline'
-    batch_size = 256
+    args = get_args()
+    
     #configure dataset
-    dataset = ImageFolder(dataset_path, transform=torchvision.transforms.ToTensor())
+    dataset = ImageFolder(args.dataset_path, transform=torchvision.transforms.ToTensor())
     total_length = len(dataset)
-    train_len = int(0.9*total_length)
+    train_len = int(args.train_size*total_length)
     test_len = total_length-train_len
     train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_len, test_len])
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size = batch_size, shuffle=True, num_workers=8,
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, num_workers=8,
                                                     pin_memory=True, persistent_workers=True, drop_last=True)
-    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size = batch_size, shuffle=False, num_workers=8,
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size = args.batch_size, shuffle=False, num_workers=8,
                                                     pin_memory=True, persistent_workers=True, drop_last=True)
     
     #convert class labels to queries/prompts
@@ -123,25 +162,40 @@ if __name__ == '__main__':
     vowels = ['a','e','i','o','u']
     prompted_mapping = {value:prompt+'a '+key.replace('_',' ') if key[0] not in vowels else prompt+'an '+key.replace('_',' ') for key,value in label_mapping.items()} 
     queries = list(prompted_mapping.values())
+    args.queries = queries
 
+    evaluation_dir = '/home/a.dhakal/active/user_a.dhakal/geoclip/logs/evaluations/zero_shot_results.txt'
+    alphas = [0,0.1,0.2,0.3]
+                
+    with open(evaluation_dir, 'a') as f:
+        to_write = f'alpha\tmean_accuracy\tLength of Test Set\n'
+        f.write(to_write)
+        f.write('_______________________________________________________\n')
     #predict zero shot labels
-    zs_model = ZeroShot(queries, ckpt_path).eval()
-    zs_model = zs_model
-    with torch.no_grad():
-        accs = []
-        for i,batch in tqdm(enumerate(test_dataloader)):
-            x,y = batch
-            pred_dict = zs_model((x,y))
-            pred_dict = pred_dict
-            preds = pred_dict['pred']
-            gt = pred_dict['gt']
-            pred_mask = preds==gt
-            acc = pred_mask.sum()/len(pred_mask) 
-            accs.append(acc.numpy())
-        print(f'Length of dataset is {batch_size*i}')
-        mean_acc = np.mean(accs)
-        print(f'The mean accuracy for zero shot classification is {mean_acc}')
-
+    
+    for alpha in alphas:
+        args.geoclip_wt = alpha
+        print(f'Using alpha {args.geoclip_wt}')
+        zs_model = ZeroShot(args).eval()
+        zs_model = zs_model
+        with torch.no_grad():
+            accs = []
+            for i,batch in tqdm(enumerate(test_dataloader)):
+                x,y = batch
+                pred_dict = zs_model((x,y))
+                pred_dict = pred_dict
+                preds = pred_dict['pred']
+                gt = pred_dict['gt']
+                pred_mask = preds==gt
+                acc = pred_mask.sum()/len(pred_mask) 
+                accs.append(acc.numpy())
+            print(f'Length of dataset is {args.batch_size*i}')
+            mean_acc = np.mean(accs)
+            print(f'The mean accuracy for zero shot classification with aplha {args.geoclip_wt} is {mean_acc}')
+            with open(evaluation_dir, 'a') as f:
+                to_write = f'{args.geoclip_wt}\t{mean_acc}\t{args.batch_size*i}\n'
+                f.write(to_write)
+                f.write('_______________________________________________________\n')
 
     #code.interact(local=dict(globals(), **locals()))
 
