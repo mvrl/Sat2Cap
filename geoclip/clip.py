fe91effca2f8c6c9e725a606e4fe03c9ff24171b
Dhakal <a.dhakal@compute1-client-1.ris.wustl.edu>
2023-03-14 16:36:56 -0500

    explicitly set the device in the clip.py script

--- geoclip/clip.py
+++ geoclip/clip.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from transformers import CLIPImageProcessor,CLIPVisionModelWithProjection, CLIPVisionConfig
 import pytorch_lightning as pl
 import imageio
+import sys
 
 class Clip(pl.LightningModule):
     def __init__(self, args, img_type):
@@ -11,7 +12,10 @@ class Clip(pl.LightningModule):
         self.args = args
         self.img_type=img_type
         self.imo_crop = 224
+        
+        self.args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.vit_map = {'32':'openai/clip-vit-base-patch32', '16':'openai/clip-vit-base-patch16', '14L':'openai/clip-vit-large-patch14'}
+        print(f'Args.vit is {args.vit}')
         self.vit = self.vit_map[args.vit]
         if self.img_type == 'ground_level':
             print(f'Ground Level Clip instantiated with {self.vit}')
@@ -19,20 +23,15 @@ class Clip(pl.LightningModule):
             self.vision_model = CLIPVisionModelWithProjection.from_pretrained(self.vit).eval()
         elif self.img_type == 'overhead':
             print(f'Overhead Clip instantiated with {self.vit}')
-            # self.config = CLIPVisionConfig.from_pretrained(self.vit)
-            # self.config.image_size = self.imo_crop
-            # self.image_processor = CLIPImageProcessor(do_resize=True,size=448, do_center_crop=True, crop_size=(self.imo_crop,self.imo_crop), padding=True)
-            # self.vision_model = CLIPVisionModelWithProjection.from_pretrained(self.vit, 
-            #                     config=self.config, ignore_mismatched_sizes=True).train()
             self.image_processor = CLIPImageProcessor.from_pretrained(self.vit)
             self.vision_model = CLIPVisionModelWithProjection.from_pretrained(self.vit).train()
         #print(self.vision_model.config)
 
     def forward(self,x):
         if self.img_type=='ground_level':
-            processed_image = self.image_processor(x, return_tensors='pt', padding=True).to(self.device)
+            processed_image = self.image_processor(x, return_tensors='pt', padding=True).to(self.args.device)
         elif self.img_type=='overhead':
-            processed_image = self.image_processor(x, return_tensors='pt', padding=True, do_resize=True).to(self.device)
+            processed_image = self.image_processor(x, return_tensors='pt', padding=True, do_resize=True).to(self.args.device)
             #print(self.processed_image.pixel_values.shape)
         batch_tensors = self.vision_model(**processed_image)
         batch_embeddings = batch_tensors.image_embeds
