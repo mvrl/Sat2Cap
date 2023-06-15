a3ae6f38e43156564391a5d43876c724dae78e61
Dhakal <a.dhakal@compute1-exec-197.ris.wustl.edu>
2023-06-15 23:06:21 +0000

    fixed generating embeddings for an entire region

--- geoclip/evaluations/generate_map_embeddings.py
+++ geoclip/evaluations/generate_map_embeddings.py
@@ -9,6 +9,10 @@ from argparse import ArgumentParser, Namespace
 from tqdm import tqdm
 import glob
 import PIL
+from PIL import ImageFile
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 #code.interact(local=dict(globals(), **locals()))
 
 #local imports
@@ -16,6 +20,25 @@ from ..models.geomoco import GeoMoCo
 from ..utils.random_seed import set_seed
 from ..multidata import MultiData
 from ..utils.preprocess import Preprocess
+from ..data.region_data import RegionDataset
+
+
+
+
+def get_lat_lon(img_paths):
+    lat_lon = []
+
+    for path in img_paths:
+        img_name = path.split('/')[-1]
+        splits = img_name.split('_')
+        lat = float(splits[0])
+        lon = float(splits[1].replace('.jpg',''))
+        lat_lon.append(np.array([lat, lon]))
+
+    return np.array(lat_lon)
+
+
+
 
 def get_args():
     parser = ArgumentParser()
@@ -25,87 +48,83 @@ def get_args():
     parser.add_argument('--test_dir', type=str, default='/home/a.dhakal/active/proj_smart/BING_IMG/netherland_bing/')
 
     parser.add_argument('--embedding_size', type=int, default=512)
-    parser.add_argument('--max_size', default=147420)
+    #parser.add_argument('--max_size', default=147420)
 
     args = parser.parse_args()
     return args
 
-def get_lat_lon(img_paths):
-    lat_lon = []
-    for path in img_paths:
-        img_name = path.split('/')[-1]
-        splits = img_name.split('_')
-        lat = float(splits[0])
-        lon = float(splits[1].replace('.jpg',''))
-        lat_lon.append(np.array([lat, lon]))
-    return np.array(lat_lon)
+
 
 if __name__ == '__main__':
     set_seed(56)
+
+    #allow loading truncated files
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
     #configure params
     args = get_args()
-    ckpt_path=args.ckpt_path
     batch_size=args.batch_size
-    test_dir = args.test_dir
 
     #find path of embeddings that already exist
-    test_paths = glob.glob(f'{test_dir}/*/*.jpg')
-    #existing_files = [path.split('/')[-1].split('.')[0] for path in existing_paths]
-    
-    #remove paths which have already been traversed over
-    print(f'Total Input Images: {len(test_paths)}')
-    assert(len(test_paths)==args.max_size)
+    test_paths = glob.glob(f'{args.test_dir}/*/*.jpg')
 
-    embedding_size=args.embedding_size
-    max_size = args.max_size
+    #create a dataset of the images
+    dataset = RegionDataset(test_paths)
+
+    #create a dataloader
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=False, num_workers=8)
+
+    print(f'Total Input Images: {len(dataset)}')
+    #max size is the number of test_path
+    max_size = len(test_paths)    
     
     device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     #initialize the h5 file
-    output_path = args.output_path
-    with h5py.File(output_path, 'w') as f:
+    
+    with h5py.File(args.output_path, 'w') as f:
         
-        print(f'Creating new h5 file:{output_path}')
-        dset_tensor = f.create_dataset('tensor', shape=(0,embedding_size), maxshape=(max_size, embedding_size), dtype=np.float32)
+        print(f'Creating new h5 file:{args.output_path}')
+        dset_tensor = f.create_dataset('tensor', shape=(0,args.embedding_size), maxshape=(max_size, args.embedding_size), dtype=np.float32)
         dset_location = f.create_dataset('location', shape=(0, 2), maxshape=(max_size, 2), dtype=np.float32)
         
 
     #load pretrained model
-    pretrained_model = GeoMoCo.load_from_checkpoint(ckpt_path).eval()
+    pretrained_model = GeoMoCo.load_from_checkpoint(args.ckpt_path).eval()
     overhead_encoder = pretrained_model.imo_encoder.eval().to(device)
-    for params in geoclip.parameters():
+    for params in overhead_encoder.parameters():
         params.requires_grad=False
 
     #load the overhead image preprocessor
     geo_processor = Preprocess()
      
      # calculate the number of steps
-    num_steps = len(test_paths)/batch_size
+    num_steps = round(len(dataset)/batch_size)
+
+
+    
     curr_start = 0 #initialize a pointer
     
-    with h5py.File(output_path, 'a') as f:
+    with h5py.File(args.output_path, 'a') as f:
         print('Adding data to h5 file')
         print(f'Running on {device}')
         dset_tensor = f['tensor']
         dset_location = f['location']
-        for i in range(num_steps):
-            curr_img_paths = test_paths[curr_start:curr_start+batch_size]
+        for i,sample in tqdm(enumerate(dataloader)):
+            processed_imgs, locations = sample
+            if len(locations)==batch_size:
+                #get the unnormalized embeddings
+                _, unnormalized_batch_embeddings = overhead_encoder.forward(processed_imgs)
+                dset_tensor[curr_start:curr_start+batch_size] = unnormalized_batch_embeddings.detach().cpu()
+                dset_location[curr_start:curr_start+batch_size] = locations
+                
+                curr_start = curr_start+batch_size
+            else:    
+                #get the unnormalized embeddings
+                _, unnormalized_batch_embeddings = overhead_encoder.forward(processed_imgs)
+                dset_tensor[curr_start:] = unnormalized_batch_embeddings.detach().cpu()
+                dset_location[curr_start:] = locations
 
-            #get lat longs for the current image
-            curr_lat_lon = get_lat_lon(curr_img_paths)
-
-            #process the current images
-            curr_pil_imgs = [PIL.Image.open(img_path) for img_path in curr_img_paths]
-            processed_imgs = geo_processor.preprocess_overhead(curr_pil_imgs)
-            
-            #get the unnormalized embeddings
-            _, unnormalized_batch_embeddings = overhead_encoder.forward(processed_imgs)
-            dset_tensor[curr_start:curr_start+batch_size] = unnormalized_batch_embeddings.detach()
-            dset_location[curr_start:curr_start+batch_size] = curr_lat_lon
-
-
-        print(f'Number of data points is {i*batch_size}')
-        print(f'File saved in {output_path}')
+        print(f'File saved in {args.output_path}')
 
     # f = h5py.File(output_path, 'r') 
     # dset_tensor = f['tensor']
--- geoclip/utils/unittest.py
+++ geoclip/utils/unittest.py
@@ -2,16 +2,24 @@ from ..data import landuse
 from ..utils import utils
 from argparse import Namespace
 from ..multidata_raw import MultiDataRaw
+from ..data.region_data import RegionDataset
 import code
+import glob
+from torch.utils.data import DataLoader
 
+# wds_path = '/home/a.dhakal/active/datasets/YFCC100m/webdataset/0a912f85-6367-4df4-aafe-b48e6e1d2be4.tar'
+# #wds_path = '/scratch1/fs1/jacobsn/a.dhakal/yfc100m/93b7d2ae-0c93-4465-bff8-40e719544440.tar'
+# args = {'vali_path':wds_path, 'val_batch_size':32, 'train_epoch_length':10, 'normalize_embeddings':True}
 
-wds_path = '/home/a.dhakal/active/datasets/YFCC100m/webdataset/0a912f85-6367-4df4-aafe-b48e6e1d2be4.tar'
-#wds_path = '/scratch1/fs1/jacobsn/a.dhakal/yfc100m/93b7d2ae-0c93-4465-bff8-40e719544440.tar'
-args = {'vali_path':wds_path, 'val_batch_size':32, 'train_epoch_length':10, 'normalize_embeddings':True}
+# args = Namespace(**args)
+# dataset = MultiDataRaw(args).get_ds('test')
+# sample = next(iter(dataset))
+# img, imo, meta, key = sample
 
-args = Namespace(**args)
-dataset = MultiDataRaw(args).get_ds('test')
-sample = next(iter(dataset))
-img, imo, meta, key = sample
-
+img_paths = glob.glob('/home/a.dhakal/active/proj_smart/BING_IMG/netherland_bing/*/*.jpg')
+print(f'Num Images: {len(img_paths)}')
+dataset = RegionDataset(img_paths)
+dataloader = DataLoader(dataset, shuffle=False, batch_size=100, num_workers=0)
+sample = next(iter(dataloader))
+img, loc = sample
 code.interact(local=dict(globals(), **locals()))
