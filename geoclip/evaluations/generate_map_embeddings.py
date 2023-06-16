de5d2f060b719e2b122bdafc9a76ab5dc541942f
Dhakal <a.dhakal@compute1-exec-197.ris.wustl.edu>
2023-06-16 02:05:01 +0000

    added attributes to the h5 file with model_path, input_region, and metadata

--- geoclip/evaluations/generate_map_embeddings.py
+++ geoclip/evaluations/generate_map_embeddings.py
@@ -44,9 +44,8 @@ def get_args():
     parser = ArgumentParser()
     parser.add_argument('--ckpt_path', type=str, default='/home/a.dhakal/active/user_a.dhakal/geoclip/logs/temp_models/s212e5he/checkpoints/step=38000-val_loss=4.957.ckpt')
     parser.add_argument('--batch_size', type=int, default=540)
-    parser.add_argument('--output_path', type=str, default='/home/a.dhakal/active/user_a.dhakal/geoclip/logs/evaluations/wacv/geoembed_embeddings/netherlands/no_dropout/step=38000-val_loss=4.957.h5')
+    parser.add_argument('--output_dir', type=str, default='/home/a.dhakal/active/user_a.dhakal/geoclip/logs/evaluations/wacv/geoembed_embeddings/netherlands/no_dropout')
     parser.add_argument('--test_dir', type=str, default='/home/a.dhakal/active/proj_smart/BING_IMG/netherland_bing/')
-
     parser.add_argument('--embedding_size', type=int, default=512)
     #parser.add_argument('--max_size', default=147420)
 
@@ -67,26 +66,33 @@ if __name__ == '__main__':
     #find path of embeddings that already exist
     test_paths = glob.glob(f'{args.test_dir}/*/*.jpg')
 
+    #extract output path
+    model_name = args.ckpt_path.split('/')[-1].split('.')[0]
+    output_path = f'{args.output_dir}/{model_name}.h5'
+
     #create a dataset of the images
     dataset = RegionDataset(test_paths)
 
     #create a dataloader
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=False, num_workers=8)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=False, num_workers=12)
 
     print(f'Total Input Images: {len(dataset)}')
     #max size is the number of test_path
-    max_size = len(test_paths)    
+    data_size = len(test_paths)    
     
     device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     #initialize the h5 file
     
-    with h5py.File(args.output_path, 'w') as f:
+    with h5py.File(output_path, 'w') as f:
         
-        print(f'Creating new h5 file:{args.output_path}')
-        dset_tensor = f.create_dataset('tensor', shape=(0,args.embedding_size), maxshape=(max_size, args.embedding_size), dtype=np.float32)
-        dset_location = f.create_dataset('location', shape=(0, 2), maxshape=(max_size, 2), dtype=np.float32)
+        print(f'Creating new h5 file:{output_path}')
+        dset_tensor = f.create_dataset('overhead_embeddings', shape=(data_size,args.embedding_size), dtype=np.float32)
+        dset_location = f.create_dataset('location', shape=(data_size, 2), dtype=np.float32)
         
+        f.attrs['model_path'] = args.ckpt_path
+        f.attrs['input_region'] = args.test_dir
+        f.attrs['general'] = 'Unnormalized Overhead Embeddings without date time loc info'
 
     #load pretrained model
     pretrained_model = GeoMoCo.load_from_checkpoint(args.ckpt_path).eval()
@@ -104,10 +110,10 @@ if __name__ == '__main__':
     
     curr_start = 0 #initialize a pointer
     
-    with h5py.File(args.output_path, 'a') as f:
+    with h5py.File(output_path, 'a') as f:
         print('Adding data to h5 file')
         print(f'Running on {device}')
-        dset_tensor = f['tensor']
+        dset_tensor = f['overhead_embeddings']
         dset_location = f['location']
         for i,sample in tqdm(enumerate(dataloader)):
             processed_imgs, locations = sample
@@ -124,7 +130,7 @@ if __name__ == '__main__':
                 dset_tensor[curr_start:] = unnormalized_batch_embeddings.detach().cpu()
                 dset_location[curr_start:] = locations
 
-        print(f'File saved in {args.output_path}')
+        print(f'File saved in {output_path}')
 
     # f = h5py.File(output_path, 'r') 
     # dset_tensor = f['tensor']
